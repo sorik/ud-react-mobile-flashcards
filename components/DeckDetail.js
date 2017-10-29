dbffe00a8db786d6b960bb396ae97c3ea5aad95e
sori-kang <sori.kang@rea-group.com>
2017-10-30 00:46:16 +1100

    feat: style detail view

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 
 class DeckDetail extends Component {
@@ -14,15 +14,69 @@ class DeckDetail extends Component {
   }
 
   render() {
-    const { title } = this.props
+    const { title, questions } = this.props
 
     return (
-      <View>
-        <Text>{title}</Text>
+      <View style={styles.container}>
+        <View style={styles.deckInfo}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.cards}>{questions.length} cards</Text>
+        </View>
+        <View style={styles.actions}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Add Card</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Start Quiz</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  deckInfo: {
+    flex: 1,
+    marginTop: 100,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  title: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    fontSize: 70,
+    textAlign: 'center'
+  },
+  cards: {
+    fontSize: 25,
+    textAlign: 'center',
+    marginBottom: 30,
+    color: 'gray'
+  },
+  actions: {
+    flex: 1
+  },
+  button: {
+    backgroundColor: 'black',
+    padding: 10,
+    borderRadius: 7,
+    height: 60,
+    marginLeft: 60,
+    marginRight: 60,
+    marginTop: 10,
+    marginBottom: 10
+  },
+  buttonText: {
+    fontSize: 30,
+    color: 'white',
+    textAlign: 'center',
+  }
+
+})
 
 function mapStateToProps(state, { navigation }){
   return state[navigation.state.params.deck]
