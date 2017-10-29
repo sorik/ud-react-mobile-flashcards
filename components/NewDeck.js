2b952e4613bebbd439819dd2d5ebe933eb764134
sori-kang <sori.kang@rea-group.com>
2017-10-30 01:13:55 +1100

    feat: Style new deck view

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,14 +1,62 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
 
 class NewDeck extends Component {
+  state = {
+    text: ''
+  }
+
   render() {
     return (
-      <View>
-        <Text>New deck</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>What is the title of your new deck?</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => this.setState({ text })}
+          value={this.state.text}
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
       </View>
     )
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  title: {
+    fontSize: 40,
+    textAlign: 'center',
+    marginTop: 30,
+    marginBottom: 30,
+    padding: 10
+  },
+  input: {
+    height: 60,
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 50,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 7
+  },
+  button: {
+    backgroundColor: 'black',
+    padding: 10,
+    borderRadius: 7,
+    height: 60,
+    marginLeft: 60,
+    marginRight: 60,
+    marginTop: 10,
+    marginBottom: 10
+  },
+  buttonText: {
+    fontSize: 30,
+    color: 'white',
+    textAlign: 'center',
+  }
+})
 export default NewDeck
